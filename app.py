291e1ecbe3fe1f3b40aff2e469ca1e780a8907be
Soomin Jung <t19257@kist.re.kr>
2019-04-23 14:26:40 +0900

    Modify requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1)
 
 if __name__ == '__main__':
     app.run(debug=True)
