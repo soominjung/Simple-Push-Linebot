8d82fa60e9977be0a19874dc2ab1793151803737
summmmn <saneu_@naver.com>
2019-04-23 19:42:16 +0900

    available to send a push message running python code

--- sendPushMessage.py
+++ sendPushMessage.py
@@ -1,59 +1,3 @@
-'''
-import requests
-
-userID = input('Enter userID: ')
-message = input('Enter message: ')
-
-headers = {
-    'userID': userID,
-    'message': message
-}
-r = requests.post('https://pushmessagevialine.herokuapp.com/', headers=headers)
-print(r.elapsed.total_seconds())
-print(r.headers)
-#print(r.json())
-
-import json
-import sys
-import os
-
-import responses
-
-from linebot import (
-    LineBotApi
-)
-from linebot.models import (
-    TextSendMessage
-)
-
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-
-
-text_message = TextSendMessage(text='Hello, world')
-message = [{"type": "text", "text": "Hello, world"}]
-
-@responses.activate
-def push_text_message():
-    responses.add(
-        responses.POST,
-        LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/push',
-        json={}, status=200
-    )
-
-    line_bot_api.push_message('U11e778e2425c196b6787ec04a198449f', text_message)
-
-push_text_message()
-'''
 import os
 import sys
 
@@ -61,12 +5,8 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+# get channel_access_token from your environment variable
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
 if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
