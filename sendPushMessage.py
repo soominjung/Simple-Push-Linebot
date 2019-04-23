1f3608cd277b53d4c8421a64d8a2801e400620fd
summmmn <saneu_@naver.com>
2019-04-23 20:35:14 +0900

    modify sendPushMessage.py

--- sendPushMessage.py
+++ sendPushMessage.py
@@ -13,4 +13,16 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 
+#try:
 line_bot_api.push_message('U11e778e2425c196b6787ec04a198449f', TextSendMessage(text='Hello World!'))
+#except LineBotApiError as e:
+#    print('e')
+'''
+while True :
+    tmp = input('Enter a message to send: ')
+    if(tmp!='q') :
+        if(isinstance(tmp,str)) :
+            line_bot_api.push_message('U11e778e2425c196b6787ec04a198449f', TextSendMessage(text='hello'))
+    else :
+        break
+'''
